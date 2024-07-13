a93e7a07976ac1405a1de9324e509bcb9b546742
Kiper02 <permakovkirill@gmail.com>
2024-07-13 21:01:32 +0500

    SUCCESS_PROJECT

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useEffect, useState } from 'react';
+import LoginForm from './components/LoginForm';
+import { Context } from './index';
+import { observer } from 'mobx-react-lite';
+import { IUser } from './models/IUser';
+import UserService from './services/UserSevice';
 
 function App() {
+  const {store} = useContext(Context)
+  const [users, setUsers] = useState<IUser[]>([])
+
+  useEffect(() => {
+    if(localStorage.getItem('token')) {
+      store.checkAuth()
+    }
+  }, [])
+
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers()
+      setUsers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if(store.isLoading) {
+    return <div>Загрузка...</div>
+  }
+
+  if(!store.isAuth) {
+    return (
+      <LoginForm />
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{store.isAuth ? `Пользователь авторизован ${store.user.email}` : 'Авторизуйтесь'}</h1>
+      <h1>{store.user.isActivate ? 'Аккаует подтвержден по почте' : 'Подтвердите аккаунт'}</h1>
+      <button onClick={() => store.logout()}>Выйти</button>
+      <div>
+        <button onClick={getUsers}>Получить пользователей</button>
+      </div>
+      {users.map(user =>
+        <div key={user.id}>{user.email}</div>
+      )}
     </div>
   );
 }
 
-export default App;
+export default observer(App);
---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,37 @@
+import React, { FC, useContext, useState } from 'react';
+import { Context } from '../index';
+import { observer } from 'mobx-react-lite';
+
+const LoginForm: FC = () => {
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const {store} = useContext(Context)
+    return (
+    <div>
+        <input 
+            type='text'
+            onChange={e => setEmail(e.target.value)}
+            value={email}
+            placeholder='Email'
+        />
+        <input 
+            type='text'
+            onChange={e => setPassword(e.target.value)}
+            value={password}
+            placeholder='Пароль'
+        />
+        <button 
+            onClick={() => store.login(email, password)}
+        >
+            Логин
+        </button>
+        <button 
+            onClick={() => store.registration(email, password)}
+        >
+            Регистрация
+        </button>
+    </div>
+    );
+};
+
+export default observer(LoginForm);
