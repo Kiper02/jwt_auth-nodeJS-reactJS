7cbdcd180b62ffbc078901d2b7b5381012a298d5
Kiper02 <permakovkirill@gmail.com>
2024-07-13 14:04:16 +0500

    smtp-success

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -1,4 +1,4 @@
-import nodemailer from 'nodemailer'
+import nodemailer from 'nodemailer';
 
 class MailService {
 
@@ -6,19 +6,20 @@ class MailService {
         this.transporter = nodemailer.createTransport({
             host: process.env.SMTP_HOST,
             port: process.env.SMTP_PORT,
-            secure: false,
+            secure: true, // true для 465, false для других портов
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASSWORD
-            }
-        })
+            },
+            connectionTimeout: 10000 // увеличить таймаут до 10 секунд
+        });
     }
 
     async sendActivationMail(to, link) {
         await this.transporter.sendMail({
             from: process.env.SMTP_USER,
             to,
-            sybject: 'Активация аккаунта на ' + process.env.SMTP_URL,
+            subject: 'Активация аккаунта на ' + process.env.SMTP_URL, // исправлена опечатка
             text: '',
             html:
                 `
@@ -27,8 +28,8 @@ class MailService {
                         <a href="${link}">${link}</a>
                     </div>
                 `
-        })
+        });
     }
 }
 
-export default new MailService()
+export default new MailService();
